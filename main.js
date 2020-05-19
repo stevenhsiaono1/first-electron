ca3f827d83ff89958be86a50a774dcfbe4939633
steven780122 <steven780122@yahoo.com.tw>
2020-05-19 08:18:20 +0800

    first learn end

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 
 app.on('ready', () =>{
     const mainWindow = new BrowserWindow({
@@ -12,17 +12,26 @@ app.on('ready', () =>{
 
     mainWindow.loadFile('index.html');
 
-    const secondWindow = new BrowserWindow({
-        width: 400,
-        height: 300,
-        webPreferences:{
-            nodeIntegration: true,   // 如此在renderer.js可以調用nodejs的API
-        },
-        parent:mainWindow   // 如此父窗口關閉  子窗口即關閉
-
+    // 測試接收從renderer的訊息
+    ipcMain.on('message', (event, arg)=>{
+        console.log(arg);    //此時可以在command看到打印出來!
+        // 收到後可以再跟sender說已收到
+        event.sender.send('reply', 'hello from main!');
     });
 
-    secondWindow.loadFile('second.html');
+
+    // 可以開兩個window
+    // const secondWindow = new BrowserWindow({
+    //     width: 400,
+    //     height: 300,
+    //     webPreferences:{
+    //         nodeIntegration: true,   // 如此在renderer.js可以調用nodejs的API
+    //     },
+    //     parent:mainWindow   // 如此父窗口關閉  子窗口即關閉
+
+    // });
+
+    // secondWindow.loadFile('second.html');
 
 
 });
--- renderer.js
+++ renderer.js
@@ -7,8 +7,19 @@
 
 
 // 可使用node API 和DOM API
-alert(process.versions.node)  // node API
+// alert(process.versions.node)  // node API
+
+// 測試向main.js 發送訊息 (透過IPC)
+const {ipcRenderer} = require('electron')
 
 window.addEventListener('DOMContentLoaded',() =>{       // DOM API 也可以實現~!
-    alert('Greeting from DOM side'); 
+    // alert('Greeting from DOM side'); 
+
+    ipcRenderer.send('message', 'hello from renderer!');   // 測試從renderer發送給
+
+    // 測試接收從renderer的訊息
+    ipcRenderer.on('reply', (event, arg)=>{
+        // 可以操作dom!!!
+        document.getElementById("message").innerHTML = arg;
+    });
 })
